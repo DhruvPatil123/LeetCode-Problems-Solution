5eb733e76b362d8f4db8f0886330122de3c38919
DHRUV PATIL <162692908+DhruvPatil123@users.noreply.github.com>
2026-06-29 19:26:14 +0530

    Time: 1 ms (78%), Space: 42.4 MB (66.24%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+        
+        // Move fast at twice the speed of slow until they meet or fast hits 1
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+        
+        return fast == 1;
+    }
+    
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            totalSum += d * d;
+            n = n / 10;
+        }
+        return totalSum;
+    }
+}
