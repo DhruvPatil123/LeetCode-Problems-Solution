634a482f87ea719d019ed54bd3a11329c4bb6279
DHRUV PATIL <162692908+DhruvPatil123@users.noreply.github.com>
2026-07-11 12:58:54 +0530

    Time: 8 ms (94.15%), Space: 47.8 MB (20.28%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<Integer>[] graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        boolean[] visited = new boolean[n];
+        int completeComponents = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                int[] info = dfs(i, graph, visited);
+
+                int nodes = info[0];
+                int edgesCount = info[1] / 2; // each edge counted twice
+
+                if (edgesCount == nodes * (nodes - 1) / 2) {
+                    completeComponents++;
+                }
+            }
+        }
+
+        return completeComponents;
+    }
+
+    private int[] dfs(int node, List<Integer>[] graph, boolean[] visited) {
+        visited[node] = true;
+
+        int nodes = 1;
+        int edges = graph[node].size();
+
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor]) {
+                int[] res = dfs(neighbor, graph, visited);
+                nodes += res[0];
+                edges += res[1];
+            }
+        }
+
+        return new int[]{nodes, edges};
+    }
+}
