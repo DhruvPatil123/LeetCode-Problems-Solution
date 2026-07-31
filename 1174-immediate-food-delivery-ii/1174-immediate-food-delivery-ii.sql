33ff70a2f532117c42d3ac0127e8ad624b2e29cd
DHRUV PATIL <162692908+DhruvPatil123@users.noreply.github.com>
2026-07-31 19:17:39 +0530

    Time: 1026 ms (8.55%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+WITH FirstOrders AS (
+    SELECT 
+        customer_id,
+        order_date,
+        customer_pref_delivery_date,
+        RANK() OVER (PARTITION BY customer_id ORDER BY order_date ASC) AS rnk
+    FROM 
+        Delivery
+)
+SELECT 
+    ROUND(
+        AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1.0 ELSE 0.0 END) * 100, 
+        2
+    ) AS immediate_percentage
+FROM 
+    FirstOrders
+WHERE 
+    rnk = 1;
