efb5c8cc18bac821fee20d5c32796037344c29e2
DHRUV PATIL <162692908+DhruvPatil123@users.noreply.github.com>
2026-07-31 19:09:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (49.36%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        
+        pushes = 0
+        k = 1  # number of pushes per letter
+        
+        while n > 0:
+            count = min(n, 8)
+            pushes += count * k
+            n -= count
+            k += 1
+            
+        return pushes
